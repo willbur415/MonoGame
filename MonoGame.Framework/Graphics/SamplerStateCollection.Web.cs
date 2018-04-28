2d8d0b718610601aaa743118e4c9048dcee21e56
cra0zy <cra0zy@gmail.com>
2018-04-28 03:33:01 +0200

    lets get some sleep

--- MonoGame.Framework/Graphics/SamplerStateCollection.Web.cs
+++ MonoGame.Framework/Graphics/SamplerStateCollection.Web.cs
@@ -1,10 +1,9 @@
 // MonoGame - Copyright (C) The MonoGame Team
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
-//
-// Author: Kenneth James Pouncey
 
 using System;
+using static WebHelper;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
@@ -31,8 +30,19 @@ namespace Microsoft.Xna.Framework.Graphics
 
                 if (sampler != null && texture != null && sampler != texture.glLastSamplerState)
                 {
-                    // TODO
+                    // TODO: Avoid doing this redundantly (see TextureCollection.SetTextures())
+                    // However, I suspect that rendering from the same texture with different sampling modes
+                    // is a relatively rare occurrence...
+                    gl.activeTexture(gl.TEXTURE0 + i);
+                    GraphicsExtensions.CheckGLError();
 
+                    // NOTE: We don't have to bind the texture here because it is already bound in
+                    // TextureCollection.SetTextures(). This, of course, assumes that SetTextures() is called
+                    // before this method is called. If that ever changes this code will misbehave.
+                    // GL.BindTexture(texture.glTarget, texture.glTexture);
+                    // GraphicsExtensions.CheckGLError();
+
+                    sampler.Activate(device, texture.glTarget, texture.LevelCount > 1);
                     texture.glLastSamplerState = sampler;
                 }
             }

df70e869f451a7899564e3eb09cc9e0a8dc7dafa
cra0zy <cra0zy@gmail.com>
2019-12-25 00:18:47 +0100

    Better cursor handling part 1

--- MonoGame.Framework/Platform/Input/Mouse.SDL.cs
+++ MonoGame.Framework/Platform/Input/Mouse.SDL.cs
@@ -3,6 +3,8 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Microsoft.Xna.Framework.Input
 {
@@ -10,6 +12,25 @@ namespace Microsoft.Xna.Framework.Input
     {
         internal static int ScrollX;
         internal static int ScrollY;
+
+        private static Dictionary<MouseCursor, IntPtr> _cursors;
+
+        static Mouse()
+        {
+            _cursors = new Dictionary<MouseCursor, IntPtr>();
+            _cursors[MouseCursor.Arrow] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.Arrow);
+            _cursors[MouseCursor.IBeam] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.IBeam);
+            _cursors[MouseCursor.Wait] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.Wait);
+            _cursors[MouseCursor.Crosshair] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.Crosshair);
+            _cursors[MouseCursor.WaitArrow] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.WaitArrow);
+            _cursors[MouseCursor.SizeNWSE] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.SizeNWSE);
+            _cursors[MouseCursor.SizeNESW] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.SizeNESW);
+            _cursors[MouseCursor.SizeWE] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.SizeWE);
+            _cursors[MouseCursor.SizeNS] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.SizeNS);
+            _cursors[MouseCursor.SizeAll] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.SizeAll);
+            _cursors[MouseCursor.No] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.No);
+            _cursors[MouseCursor.Hand] = Sdl.Mouse.CreateSystemCursor(Sdl.Mouse.SystemCursor.Hand);
+        }
 
         private static IntPtr PlatformGetWindowHandle()
         {
@@ -57,9 +78,43 @@ namespace Microsoft.Xna.Framework.Input
             Sdl.Mouse.WarpInWindow(PrimaryWindow.Handle, x, y);
         }
 
-        private static void PlatformSetCursor(MouseCursor cursor)
+        private static void PlatformSetCursor(MouseCursor cursor, Texture2D texture = null, int originx = 0, int originy = 0)
         {
-            Sdl.Mouse.SetCursor(cursor.Handle);
+            if (cursor != MouseCursor.Texture)
+            {
+                Sdl.Mouse.SetCursor(_cursors[cursor]);
+                return;
+            }
+
+            var handle = IntPtr.Zero;
+            var surface = IntPtr.Zero;
+
+            if (_cursors.TryGetValue(cursor, out handle) && handle != IntPtr.Zero)
+            {
+                Sdl.Mouse.FreeCursor(handle);
+                handle = IntPtr.Zero;
+            }
+
+            try
+            {
+                var bytes = new byte[texture.Width * texture.Height * 4];
+                texture.GetData(bytes);
+                surface = Sdl.CreateRGBSurfaceFrom(bytes, texture.Width, texture.Height, 32, texture.Width * 4, 0x000000ff, 0x0000FF00, 0x00FF0000, 0xFF000000);
+                
+                if (surface == IntPtr.Zero)
+                    throw new InvalidOperationException("Failed to create surface for mouse cursor: " + Sdl.GetError());
+
+                handle = Sdl.Mouse.CreateColorCursor(surface, originx, originy);
+            }
+            finally
+            {
+                if (surface != IntPtr.Zero)
+                    Sdl.FreeSurface(surface);
+
+                if (handle != IntPtr.Zero)
+                    Sdl.Mouse.SetCursor(handle);
+            }
+
         }
     }
 }
